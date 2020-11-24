668764a5aacb42e4e64a1a572fd8cd1de62648af
root <root@ip-172-31-2-202.us-east-2.compute.internal>
2020-11-24 02:51:22 +0000

    sdfyvesj

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -23,7 +23,7 @@ class Pipeline {
 
 	def notifyFailed() {
   		emailext (
-			to:"${config.notification.email.to}"
+			to:"${config.notification.email.to}",
       			subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
       			body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
         		<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
