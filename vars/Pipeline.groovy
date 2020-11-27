f86d23487071cc3b69bc8dd2b91c1ba182eab27f
rahulyerva <42656632+rahulyerva@users.noreply.github.com>
2020-11-27 10:51:30 -0700

    Update Pipeline.groovy

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,5 +1,5 @@
 package com.example
-import groovy.yaml.*
+import groovy.yaml.YamlSlurper
 
 class Pipeline {
     def script
@@ -9,7 +9,15 @@ class Pipeline {
         this.script = script
         this.configurationFile = configurationFile
     }
-	
+    def notifyFailed(emailId) {
+  	emailext (
+		to:$emailId,
+      		subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+      		body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        	<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+    		 )
+	}
+
     def execute() {
 //    ===================== Your Code Starts Here =====================
 //    Note : use "script" to access objects from jenkins pipeline run (WorkflowScript passed from Jenkinsfile)
@@ -19,26 +27,16 @@ class Pipeline {
 	def yaml = new YamlSlurper()
 	def file = new File(configurationFile)
 	def config = yaml.parseText(file.text)
-	
-
 	def tests=[:]
-
-	
-
 	tests["performance"] = {
   		stage ("performance"){    
     			node('label_example1') {  
         			try{
-					sh "cd ${config.test[0].testFolder}"
+					sh "cd ${env.WORKSPACE}/${config.test[0].testFolder}"
 					sh "${config.test[0].testCommand}"
 				}
 				catch(e){
-					emailext (
-			to:"${config.notification.email.to}",
-      			subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-      			body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-        		<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-    			)
+					notifyFailed(${config.notification.email.to})
 				}
     			}
   		}
@@ -47,17 +45,12 @@ class Pipeline {
   		stage ("regression"){    
     			node('label_example2') {  
         			try{
-					sh "cd ${config.test[1].testFolder}"
+					sh "cd ${env.WORKSPACE}/${config.test[1].testFolder}"
 					sh "${config.test[1].testCommand}"
 					
 				}
 				catch(e){
-					emailext (
-			to:"${config.notification.email.to}",
-      			subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-      			body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-        		<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-    			)
+					notifyFailed(${config.notification.email.to})
 				}
 				
     			}
@@ -67,80 +60,55 @@ class Pipeline {
   		stage ("integration"){    
     			node('label_example2') {  
         			try{
-					sh "cd ${config.test[2].testFolder}"
+					sh "cd ${env.WORKSPACE}/${config.test[2].testFolder}"
 					sh "${config.test[2].testCommand}"
 				}
 				catch(e){
-					emailext (
-			to:"${config.notification.email.to}",
-      			subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-      			body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-        		<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-    			)
+					notifyFailed(${config.notification.email.to})
 				}
     			}
   		}
 	}
-	
+//    ===================== Run pipeline stages =======================	
 	stages{
 		script.stage('build'){
 				try{
 					def projectFolder=config.build.projectFolder
-					sh "cd ${projectFolder}"
+					sh "cd ${env.WORKSPACE}/${projectFolder}"
 					sh "${config.build.buildCommand}"
-					sh "cd .."
 				}
 				catch(e){
-					emailext (
-			to:"${config.notification.email.to}",
-      			subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-      			body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-        		<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-    			)
+					notifyFailed(${config.notification.email.to})
 				}
 			
 		}
 		script.stage('database'){
 				try{
 					def databaseFolder=config.database.databaseFolder
-					sh "cd ${databaseFolder}"
+					sh "cd ${env.WORKSPACE}/${databaseFolder}"
 					sh "${config.database.databaseCommand}"
-					sh "cd .."
 				}			
 				catch(e){
-					emailext (
-			to:"${config.notification.email.to}",
-      			subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-      			body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-        		<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-    			)
+					notifyFailed(${config.notification.email.to})
 				}
 			
 		}
 		script.stage('deploy'){
 				try{
-					//add deploy folder to config file as 'project'
 					def deployFolder=config.build.buildFolder
-					sh "cd ${deployFolder}"
+					sh "cd ${env.WORKSPACE}/${deployFolder}"
 					sh "${config.deploy.deployCommand}"
-					sh "cd .."
 				}
 				catch(e){
-					emailext (
-			to:"${config.notification.email.to}",
-      			subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-      			body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-        		<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-    			)
+					notifyFailed(${config.notification.email.to})
 				}
 			
 		}
 		parallel tests
 	}
 
-//    ===================== Run pipeline stages =======================
+
 
 //    ===================== End pipeline ==============================
     }
 }
-
